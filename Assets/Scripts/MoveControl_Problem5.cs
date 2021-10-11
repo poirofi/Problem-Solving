ade0bc711e1c186d5b4465cc0ea1e5b13141719a
poirof <poirofirdaus@gmail.com>
2021-10-11 22:11:19 +0700

    Problem5

---
+++ Assets/Scripts/MoveControl_Problem5.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl_Problem5 : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // Bola mengikuti kursor
+    public void cursor()
+    {
+        Vector2 follow = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, follow, 5 * Time.deltaTime);
+
+    }
+    private void Update()
+    {
+        cursor();
+    }
+}
---
+++ Assets/Scripts/MoveController_problem4.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController_problem4 : MonoBehaviour
+{
+    public float speed;
+    private void Update()
+    {
+        transform.Translate(
+            Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+            Input.GetAxis("Vertical") * speed * Time.deltaTime, 0f);
+    }
+}
