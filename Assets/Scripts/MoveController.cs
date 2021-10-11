2d2555c92d608862129aebe8a09bb9b92e87096e
poirof <poirofirdaus@gmail.com>
2021-10-11 21:52:52 +0700

    Problem4

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        Movement();
+    }
+
+    // untuk pergerakan bolanya ke arah random dengan kecepatan konstan
+    void Movement()
+    {
+        float x = Random.Range(-100, 100);
+        float y = Random.Range(-100, 100);
+
+        rigidBody.AddForce(new Vector2(x, y).normalized * 50);
+    }
+}
