b7af6f4a22603183deacf6f84f0bc9042c20b227
poirof <poirofirdaus@gmail.com>
2021-10-11 23:10:34 +0700

    Problem7

---
+++ Assets/Scripts/Score_Problem7.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score_Problem7 : MonoBehaviour
+{
+    public Text ScoreText;
+    private int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+        ScoreText.text = "Score : " + Score;
+    }
+
+    private void OnTriggerEnter2D(Collider2D Food)
+    {
+        if ( Food.tag == "Eat")
+        {
+            Score += 1;
+            Destroy(Food.gameObject);
+            ScoreText.text = "Score" + Score;
+        }
+    }
+}
